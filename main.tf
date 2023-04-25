8307c9ad714e4b08bf249ec3844dca5e6b5a6077
Sylvain K <53495850+skamdem@users.noreply.github.com>
2023-04-25 10:10:58 +0100

    added resource for aws_instance

--- main.tf
+++ main.tf
@@ -8,5 +8,15 @@ terraform {
 }
 
 provider "aws" {
-  # Configuration options
+  profile = "default"
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_temporary_server" {
+  ami = ""
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleServerInstance"
+  }
 }
