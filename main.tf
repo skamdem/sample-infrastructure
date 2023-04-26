9b2a62a25f386fa71b38a4c4156ea5511926cc5d
Sylvain K <53495850+skamdem@users.noreply.github.com>
2023-04-26 18:42:07 +0100

    added .gitignore

--- main.tf
+++ main.tf
@@ -1,22 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.64.0"
     }
   }
 }
 
-provider "aws" {
-  profile = "default"
-  region = "us-east-1"
-}
-
-resource "aws_instance" "my_temporary_server" {
-  ami = ""
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleServerInstance"
-  }
+locals {
+  project_name = "temp_project"
 }
